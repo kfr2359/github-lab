70844ba8a5fbbb9cbd15b387777f86fcd49afd07
Sergei Shabalov <kfr2359@gmail.com>
2025-12-09 19:04:28 +0400

    task 6 outputs

---
+++ base/outputs.tf
@@ -0,0 +1,16 @@
+output "organization_id" { value = github_organization_settings.my_org.id }
+output "teams_ids" {
+  value = [
+    github_team.nodejs_team.id,
+    github_team.devops_team.id,
+    github_team.security_team.id,
+  ]
+}
+output "security_manager_team_slug" { value = github_team.security_team.id }
+output "secret_names" {
+  value = [
+    github_actions_organization_secret.org_username.secret_name,
+    github_actions_organization_secret.org_password.secret_name,
+  ]
+}
+output "base_repository_name" { value = github_repository.tf_github_lab_base_repository.name }
